2e0f2c54b29eebb388a8dbba81ba0cd2fb63b5c5
Erik Anderson <kirenosredna@hotmail.com>
2025-02-03 22:29:53 +0000

    first file

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Author: EJA
+# Version: 0.1
+# Description: This is our first script
+
+name = "Erik Anderson"
+
+print(f'My name is {name}')
+print("My name is", name)
+print("My name is " + name)
+
+import random
+print("Lucky number is", random.randint(1,50))
+
+
+import math
+print("Cosine of 0.5 is", math.cos(0.5))
