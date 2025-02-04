c351830e079809eb6f2ffdc9391bb77c07906262
Erik Anderson <kirenosredna@hotmail.com>
2025-02-04 01:29:21 +0000

    adding ex2_2.py

---
+++ ex2_2.py
@@ -0,0 +1,5 @@
+var = input("Please enter a value: ")
+
+print(f"The value of var {var.upper()} as uppercase")
+print(f"The length of var is {len(var)}")
+print(f"Is the var decimal? {var.isdecimal()}")
