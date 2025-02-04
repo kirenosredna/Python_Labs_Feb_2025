b585324bf0a43c45a1f4d389878a1756753333ef
Erik Anderson <kirenosredna@hotmail.com>
2025-02-04 18:54:31 +0000

    added demo_platform.py

---
+++ demo_platform.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# Author: Erik Anderson
+# Description: This script will demo how to check which platform the script is
+# running on.
+"""
+    Docstring:
+"""
+
+import sys
+import os
+
+print(sys.platform)
+if sys.platform == "win32":
+    home_dir = os.environ["HOMEPATH"]
+else:
+    home_dir = os.environ["HOME"]
+
+print(home_dir)
