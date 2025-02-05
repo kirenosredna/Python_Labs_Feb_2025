9a55c5dd7ff4c39038d5909602d70ed101783cfb
Erik Anderson <kirenosredna@hotmail.com>
2025-02-05 19:50:02 +0000

    grouping funtions

--- demo_regex.py
+++ demo_regex.py
@@ -25,15 +25,16 @@ for line in fh:
     #m = re.search(r"[0-9][0-9]", line)  # returns NoneType(False) or REmatch(True)
     #m = re.search(r"\.", line)
     #m = re.search(r"^[A-Z].*[A-Z]$", line)  # returns NoneType(False) or REmatch(True)
-    # m = re.search("^[A-Z].{4}[A-Z]$", line)
+    #m = re.search("^[A-Z].{4}[A-Z]$", line)
     #m = re.match("[A-Z].{4}[A-Z]$", line) # auto matches lines starting don't need anchor
     #m = re.fullmatch("^[A-Z].{4}[A-Z]\n$", line) # has to match entire line including hidden chars!
     # m = re.search(r"^(.)(.).\2\1$", line) # use r" " for raw get in habit
-    # m = re.search(r"^([A-Z]).*\1$", line) # lines start end with same capital
-    m = re.search(r"^[A-Z]{1,2}\d{1,2}[A-Z]?\s\d[A-Z]{2}$", line) # UK Postcode regex
+    m = re.search(r"^([A-Z]).*\1$", line) # lines start end with same capital
+    #m = re.search(r"^[A-Z]{1,2}\d{1,2}[A-Z]?\s\d[A-Z]{2}$", line, flags=re.IGNORECASE) # UK Postcode regex
 
     # Alternation (OR)
     #"donald|dcameron|Sir D Cameron"
     # a (bottle|glass) of (beer|wine|lager)"
     if m:
-        print(line, end="")
+        print(f"Matched {m.group()} on {line.rstrip()} at {m.start()} - {m.end()} ,"
+              f"Groupings = {m.groups()} , Group 1  = {m.group(1)}")
