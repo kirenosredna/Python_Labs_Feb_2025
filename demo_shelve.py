0ad49e19f6767488607959eaca55dcfe68ee0f91
Erik Anderson <kirenosredna@hotmail.com>
2025-02-05 23:10:23 +0000

    pickle and shelve

--- demo_shelve.py
+++ demo_shelve.py
@@ -4,3 +4,29 @@
 """
     Docstring:
 """
+import shelve
+
+movies = { 'william': ['fury', 'pulp fiction', 'shawshank redemption'],
+           'kirill': ['pulp fiction', 'the departed', 'snatch'],
+           'mark': ['fifth element', 'armageddon', 'slapshot'],
+           'erik': ['star wars', 'blue velvet', 'dune']
+         }
+
+tv_series = { 'william': ['fame', 'walking dead'],
+              'kirill': ['got', 'outlander'],
+              'mark': ['cagney and lacey', 'scooby doo']
+              }
+
+books = { 'william': [ 'the hobbit'],
+           'kirill': ['lotr'],
+          'mark': ['world war z']
+          }
+
+with shelve.open(r"movies") as db:
+    db['movies'] = movies
+    db['tv_series'] = tv_series
+    db['books'] = books
+
+with shelve.open(r"movies") as db:
+    print(f"{db['movies']['william']}")
+    print(f"{db['tv_series']['kirill'][0]}")
