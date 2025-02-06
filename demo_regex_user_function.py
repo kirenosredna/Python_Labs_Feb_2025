1436f52919d6e8aa430f2b51ff2c4cb0e71d2aba
Erik Anderson <kirenosredna@hotmail.com>
2025-02-06 21:07:37 +0000

    cprofile example to console and file

--- demo_regex_user_function.py
+++ demo_regex_user_function.py
@@ -9,9 +9,10 @@ import re
 def search_pattern(pattern=r"^.{19}$", file=r"c:\labs\words"):
     lines = 0
     fh_in = open(file, mode="rt")
-
+    reobj = re.compile(pattern)
     for line in fh_in:
-        m = re.search(pattern, line)
+        #m = re.search(pattern, line) # compile pattern
+        m = reobj.search(line)
 
         if m:
             lines += 1
@@ -19,6 +20,15 @@ def search_pattern(pattern=r"^.{19}$", file=r"c:\labs\words"):
 
     return lines
 
-search_pattern()
-num_lines = search_pattern(r"^([A-Z]).*\1$", r"c:\labs\words")
-print(f"Matched {num_lines} lines")
+def main():
+
+    search_pattern()
+    num_lines = search_pattern(r"^([A-Z]).*\1$", r"c:\labs\words")
+    print(f"Matched {num_lines} lines")
+    return None
+
+
+if __name__ == "__main__":
+    import cProfile
+    cProfile.run("main()") #Profile/analyse to console
+    #cProfile.run("main()", "stats.prof") #output to file and run
